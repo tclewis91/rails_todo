8454cc851001e86c5ea5c72c6440b2cca9ce4cf6
tclewis91 <tclewis91@gmail.com>
2015-06-04 08:44:19 -0500

    syntax error fixed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,31 +7,32 @@ class TodosController < ApplicationController
     render json: Todo.new
   end
 
-  # def create
-  # begin
-  #   todoapp = Todo.create(tasks: params.fetch[:tasks]),
-  #   render json: todoapp, status: 200
-  # rescue ActionController::ParameterMissing => error
-  #     render json: { error: error.message }, status: 422
-  #   end
-  # end
-
-  def view
+  def create
   begin
-    render json: Todo.find(params[:id ])
-  rescue ActiveRecord::RecordNotFound => error
-    render json: { error: error.message }, status: 404
-    end
-  end
-
-  def destroy
-    if Todo.exists?(params[:id])
-      Todo.destroy(params[:id])
-      render json: { message: 'destroyed'}, status: 202
-    else
-      render json: { error: 'Task not found' }, status: 404
+    todoapp = Todo.create(tasks: params[:tasks])
+    render json: todoapp, status: 200
+  rescue ActionController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
     end
   end
 end
+
+  # def view
+  # begin
+  #   render json: Todo.find(params[:id ])
+  # rescue ActiveRecord::RecordNotFound => error
+  #   render json: { error: error.message }, status: 404
+  #   end
+  # end
+
+  # def destroy
+  #   if Todo.exists?(params[:id])
+  #     Todo.destroy(params[:id])
+  #     render json: { message: 'destroyed'}, status: 202
+  #   else
+  #     render json: { error: 'Task not found' }, status: 404
+  #   end
+# end
+# end
 
 
