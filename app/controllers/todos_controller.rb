d988edb8a0973aee4b03265d01279d35791d6d14
tclewis91 <tclewis91@gmail.com>
2015-06-04 08:45:44 -0500

    uncommented

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,22 +17,22 @@ class TodosController < ApplicationController
   end
 end
 
-  # def view
-  # begin
-  #   render json: Todo.find(params[:id ])
-  # rescue ActiveRecord::RecordNotFound => error
-  #   render json: { error: error.message }, status: 404
-  #   end
-  # end
+  def view
+  begin
+    render json: Todo.find(params[:id ])
+  rescue ActiveRecord::RecordNotFound => error
+    render json: { error: error.message }, status: 404
+    end
+  end
 
-  # def destroy
-  #   if Todo.exists?(params[:id])
-  #     Todo.destroy(params[:id])
-  #     render json: { message: 'destroyed'}, status: 202
-  #   else
-  #     render json: { error: 'Task not found' }, status: 404
-  #   end
-# end
-# end
+  def destroy
+    if Todo.exists?(params[:id])
+      Todo.destroy(params[:id])
+      render json: { message: 'destroyed'}, status: 202
+    else
+      render json: { error: 'Task not found' }, status: 404
+    end
+end
+end
 
 
